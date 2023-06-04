bddf4830f75625d764a71121c42279b419035e73
macSro <maciej.sroczek@gmail.com>
2023-06-04 14:52:26 +0200

    Flutter: Research scenario 1 implemented.

---
+++ flutter_list/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Research scenario 1'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _filter = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final listGen = List.generate(10000, (index) => index);
+
+    final list = _filter
+        ? listGen.where((element) => element % 6 == 0).toList()
+        : listGen;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Switch(
+              value: _filter,
+              onChanged: (bool value) => setState(() {
+                _filter = value;
+              }),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (context, index) => Card(
+                  child: ListTile(
+                    title: Text('List item #${list[index]}'),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
