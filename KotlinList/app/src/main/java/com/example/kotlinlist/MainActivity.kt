b2a1813379deedac2738377da3d06bb89876044a
macSro <maciej.sroczek@gmail.com>
2023-06-10 11:46:35 +0200

    Kotlin: Research scenario 1 finished.

---
+++ KotlinList/app/src/main/java/com/example/kotlinlist/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.kotlinlist
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import com.example.kotlinlist.ui.theme.KotlinListTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinListTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Content()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Content() {
+    val filterState = remember { mutableStateOf(false) }
+    
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Research scenario #1") }
+            )
+        }
+    ) { contentPadding ->
+        Box(modifier = Modifier.padding(contentPadding)) {
+            Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
+                FilterSwitch(state = filterState)
+                FilteredList(state = filterState)
+            }
+        }
+    }
+
+}
+
+@Composable
+fun FilterSwitch(state: MutableState<Boolean>){
+    Switch(
+        checked = state.value,
+        onCheckedChange = { value -> state.value = value},
+        modifier = Modifier.padding(bottom = 16.dp)
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilteredList(state: MutableState<Boolean>){
+    val list = (0..9999).toList()
+    var filteredList: List<Int>
+    val filter = state.value
+    filteredList = if(!filter){
+        list
+    } else{
+        val resultList = list.filter { it % 6 == 0}
+        resultList
+    }
+    
+    LazyColumn(modifier = Modifier.fillMaxWidth()){
+        items(filteredList){item ->
+            Card(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                ListItem(
+                    headlineText = { Text(text = "List item #$item") }
+                )
+            }
+        }
+    }
+}
